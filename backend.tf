bbfd49441418d3823d23826a7df6be67bb26aa4a
Dorisakudo <dorisakudo09@gmail.com>
2024-07-01 17:59:58 +0100

    s3 updated

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "infra/terraformstatefile"
-    bucket         = "djtech-f8a889da47d6b432"
+    bucket         = "djtech-9ba2c0ecf6639220"
     region         = "eu-west-2"
     dynamodb_table = "finance-state-locking"
   }
